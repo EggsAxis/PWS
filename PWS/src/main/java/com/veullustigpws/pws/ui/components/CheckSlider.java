bfce3044c962430bfc760129d06e83a118d39da7
Samuellustig69 <samuellustig12@gmail.com>
2023-11-15 20:09:26 +0100

    Checksliders done

--- PWS/src/main/java/com/veullustigpws/pws/ui/components/CheckSlider.java
+++ PWS/src/main/java/com/veullustigpws/pws/ui/components/CheckSlider.java
@@ -2,6 +2,8 @@ package com.veullustigpws.pws.ui.components;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
@@ -10,18 +12,22 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.AbstractButton;
 
+import com.veullustigpws.pws.app.ColorPalet;
+import com.veullustigpws.pws.resources.fonts.AppFonts;
+import com.veullustigpws.pws.utils.GUIUtils;
+
 public class CheckSlider extends AbstractButton implements MouseListener {
 	private static final long serialVersionUID = -3508736475874392659L;
 	
 	private static final String ON_TEXT = "ON";
 	private static final String OFF_TEXT = "OFF";
 	
-	private static final Dimension dimension = new Dimension(45, 22);
+	private static final Dimension dimension = new Dimension(45, 20);
 	
 	private boolean selected;
 	
 	public CheckSlider() {
-		this.setMaximumSize(dimension);
+		GUIUtils.setComponentSize(this, dimension);
 		this.addMouseListener(this);
 	}
 	
@@ -38,9 +44,30 @@ public class CheckSlider extends AbstractButton implements MouseListener {
 		Graphics2D g = (Graphics2D) graphics;
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		
-		if (selected) 	g.setColor(Color.green);
-		else 			g.setColor(Color.red);
+		if (selected) 	g.setColor(new Color(87, 152, 255)); //Dark blue color
+		else 			g.setColor(ColorPalet.RedButton);
 		g.fillRoundRect(0, 0, dimension.width, dimension.height, dimension.height, dimension.height);
+		
+		
+		
+		//circles in checkslider
+		g.setColor(ColorPalet.DarkBackgroundColor);
+		if (selected) g.fillOval(((dimension.width/4)*3) -9 ,(dimension.height/2) -9 , 18, 18);
+		else g.fillOval((dimension.width/4) -9 ,(dimension.height/2) -9 , 18, 18);
+		
+		//text in checkslider
+		FontMetrics fm = g.getFontMetrics();
+		int textXOff = ((dimension.width - fm.stringWidth(getText())) / 2);
+		int textYOff = ((dimension.height - fm.getHeight()) / 2) + fm.getAscent();
+		
+		int textXOn = ((dimension.width - fm.stringWidth(getText())) / 6);
+		int textYOn = ((dimension.height - fm.getHeight()) / 2) + fm.getAscent();
+		
+		if (selected) g.drawString("ON", textXOn, textYOn);
+		else g.drawString("OFF", textXOff, textYOff);
+		
+		
+		
 	}
 	
 	
--- PWS/src/main/java/com/veullustigpws/pws/ui/components/onbuttonpng.java
+++
@@ -1,5 +0,0 @@
-package com.veullustigpws.pws.ui.components;
-
-public class onbuttonpng {
-
-}
